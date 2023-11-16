12a38ea6030d061ce3da3aabef0212bbb307250d
fishboy4 <129198588+fishboy4@users.noreply.github.com>
2023-11-16 12:53:47 +0200

    added movement script

---
+++ Game/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private float horizontal;
+    private float vertical;
+    private float moveLimiter = 0.7f;
+
+    public float moveSpeed = 20.0f;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Gives a value between -1 and 1
+        horizontal = Input.GetAxisRaw("Horizontal"); // -1 is left
+        vertical = Input.GetAxisRaw("Vertical"); // -1 is down
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+}
