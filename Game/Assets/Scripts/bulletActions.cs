01d30573d7c292d1b9347553cf6a87ba1ec7aace
fishboy4 <129198588+fishboy4@users.noreply.github.com>
2023-11-17 23:22:07 +0200

    changed sound settings

--- Game/Assets/Scripts/bulletActions.cs
+++ Game/Assets/Scripts/bulletActions.cs
@@ -9,7 +9,7 @@ public class bulletActions : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 2f);
         Destroy(gameObject);
+        Destroy(effect, 2f);
     }
 }
