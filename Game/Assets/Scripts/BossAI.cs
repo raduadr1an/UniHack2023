27feb3bcc8cf7eb56bbbc36d8a0a453c150582d5
fishboy4 <129198588+fishboy4@users.noreply.github.com>
2023-11-19 05:53:12 +0200

    fixed boss

--- Game/Assets/Scripts/BossAI.cs
+++ Game/Assets/Scripts/BossAI.cs
@@ -5,30 +5,17 @@ using static UnityEngine.GraphicsBuffer;
 
 public class BossAI : MonoBehaviour
 {
-    public GameObject projectilePrefab;
-    public float projectileSpeed = 5.0f; 
-    public int hitCount = 10;
-    public float speed = 5.0f;
-    private Transform player;
-    public Transform Target;
-    public float shootInterval = 1.0f;
-    private float shootTimer = 0.0f;
+    public GameObject bulletPrefab; // Assign your bullet prefab in the Unity Editor
+    public Transform firePoint; // The point where the bullets will be spawned
 
-    void Start()
+    public float bulletSpeed = 10f; // Speed of the bullets
+    public int bulletCount = 5; // Number of bullets to shoot
+    public float spreadAngle = 30f; // Spread angle in degrees
+    private float shootTimer = 0f;
+    public float shootInterval = 2.0f;
+
+    private void Update()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
-
-    void Update()
-    {
-        //Vector2 direction = player.position - transform.position;
-        //transform.position += (Vector3)direction.normalized * speed * Time.deltaTime;
-
-        if (hitCount <= 0)
-        {
-            Destroy(gameObject);
-        }
-
         if (Time.time - shootTimer >= shootInterval) //If the time elapsed is more than the fire rate, allow a shot
         {
             ShootProjectile();
@@ -36,30 +23,23 @@ public class BossAI : MonoBehaviour
         }
     }
 
-    public void TakeDamage(int damage)
-    {
-        hitCount -= 1; 
-    }
-
     public void ShootProjectile()
     {
-        int numberOfProjectiles = 5;
-        float spreadAngle = 45f;
-        float startAngle = -spreadAngle / 2;
-
-        for (int i = 0; i < numberOfProjectiles; i++)
+        for (int i = 0; i < bulletCount; i++)
         {
-            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
-            Vector2 directionToPlayer = (player.position - transform.position).normalized;
-            float angle = startAngle + spreadAngle * i / (numberOfProjectiles - 1);
-            Vector2 direction = Quaternion.Euler(0, 0, angle) * directionToPlayer;
-            projectile.GetComponent<Rigidbody2D>().AddForce(direction * projectileSpeed);
+            float angle = (i - (bulletCount - 1) / 2f) * spreadAngle / (bulletCount - 1);
+
+            // Calculate direction based on the angle
+            Vector3 direction = Quaternion.Euler(0, 0, angle) * transform.right;
+
+            // Instantiate the bullet at the fire point
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+
+            // Get the Rigidbody2D component of the bullet
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+
+            // Shoot the bullet in the calculated direction
+            bulletRb.velocity = direction.normalized * bulletSpeed;
         }
-        // Calculate the direction to the target
-        Vector2 direction1 = Target.position - transform.position;
-        // Calculate the angle to the target
-        float angle1 = Mathf.Atan2(direction1.y, direction1.x) * Mathf.Rad2Deg;
-        // Rotate the watchtower to face the target
-        transform.rotation = Quaternion.Euler(0, 0, angle1);
     }
 }
